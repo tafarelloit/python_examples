2e5939f827346fddf7c19a0dc38e11f19548137c
tafarelloit <tafarello.it@gmail.com>
2021-08-21 21:52:31 -0300

    first Commit

---
+++ ExamplesStringFormatting.py
@@ -0,0 +1,31 @@
+
+name = "Itamar"
+final_greeting = "How are you {}?"
+itamar_greeting = final_greeting.format(name)
+
+print(itamar_greeting)
+
+
+name = "Antonio"
+itamar_greeting = final_greeting.format(name)
+
+print(itamar_greeting)
+
+
+
+name = "Tafarello"
+
+itamar_greeting = f"How are you {name}?"
+
+print(itamar_greeting)
+
+
+name = "Tony Cloud"
+print(itamar_greeting)
+
+
+personname = "Tony Cloud"
+final_greeting = "How are you, {name}?"
+tony_greeting = final_greeting.format(name=personname)
+
+print(tony_greeting)
